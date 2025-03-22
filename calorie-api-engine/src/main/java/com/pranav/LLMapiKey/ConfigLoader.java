31c0211cdc000f4214b35ce9f98ba682874f8225
Pranav Aggarwal <pranav3.intern@phonepe.com>
2025-03-23 04:08:15 +0530

    configLoader file

---
+++ calorie-api-engine/src/main/java/com/pranav/LLMapiKey/ConfigLoader.java
@@ -0,0 +1,18 @@
+package com.pranav.LLMapiKey;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ConfigLoader {
+    private static final String API_KEY_PATH = "Add the API_KEY";
+
+    public static String loadApiKey() {
+        try {
+            return new String(Files.readAllBytes(Paths.get(API_KEY_PATH))).trim();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load API key from file: " + API_KEY_PATH, e);
+        }
+    }
+
+}
